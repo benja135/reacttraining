a6aa905b92eb39754cd4ba45fe5407cf270b862c
benja135 <benja135@gmail.com>
2020-09-09 12:50:51 +0200

    Updates

---
+++ precache-manifest.d0489dee1abaa0a26206af99a2b79e47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a4951fccaaf825539442901fbf466b",
+    "url": "/reacttraining/index.html"
+  },
+  {
+    "revision": "59da37ba95c9a48a62a0",
+    "url": "/reacttraining/static/css/main.e209f757.chunk.css"
+  },
+  {
+    "revision": "00ec7938b01f1471b9db",
+    "url": "/reacttraining/static/js/2.020c6741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttraining/static/js/2.020c6741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59da37ba95c9a48a62a0",
+    "url": "/reacttraining/static/js/main.33050a06.chunk.js"
+  },
+  {
+    "revision": "03993d6bc8419e2b05aa",
+    "url": "/reacttraining/static/js/runtime-main.1bcb0c6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttraining/static/media/logo.5d5d9eef.svg"
+  }
+]);
